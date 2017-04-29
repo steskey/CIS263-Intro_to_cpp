6fbcf4cb79d7944af4a5e30305255231324fc294
Ira Woodring <irawoodring@gmail.com>
2017-04-29 09:13:14 -0400

    Add putStringsInVector and tests.

--- include/my_functions.h
+++ include/my_functions.h
@@ -1,4 +1,13 @@
 #ifndef __H_MY_FUNCTIONS__
 #define __H_MY_FUNCTIONS__
 
+#include <vector>
+#include <string>
+
+std::string createString();
+std::vector<std::string> createVector();
+int putStringsInVector(int num);
+
+
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,27 @@
 
 // Test cases take two parameters - the first is a test
 // name and the second is a tag.
+// This test merely checks that 1 is equal to 1.  It
+// should always pass.  It is included here to show how
+// to write test cases.
+
 TEST_CASE( "My first test", "[testing, my_tests]" ){
+	// REQUIRE causes the execution to stop
+	// if the test fails.
 	REQUIRE( 1 == 1 );
 }
 
+// The following test checks to see if the createString
+// function works correctly.
+
+TEST_CASE( "Testing createString", "[createString]" ){
+	// CHECK will run the test but continue
+	// checking other tests regardless of whether
+	// or not this test passes.
+	CHECK( createString() == "Hello world!");
+} 
+
+TEST_CASE( "Testing createVector", "[createVector]" ){
+	std::vector<std::string> my_vector = createVector();
+	CHECK( my_vector.size() == 3 );
+}
