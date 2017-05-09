f566d8c1dcd62538cb5037c096ba22d38579d39f
Samantha Teskey <teskeys@dc04.cis.gvsu.edu>
2017-05-09 12:05:11 -0400

    Added skeleton functions to source file

---
+++ src/my_functions.cpp
@@ -0,0 +1,16 @@
+#include "my_functions.h":
+
+std::string createString(){
+
+}
+
+std::vector<std::string> createVector(){
+}
+
+int putStringsInVector(int num){
+}
+
+void modifyVector(std::vector<int> & vector){
+
+}
+
