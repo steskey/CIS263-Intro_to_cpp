5b8bae47ec125ba8ed01984e2c89c24904ea67d7
Samantha Teskey <teskeys@mail.gvsu.edu>
2017-05-09 12:42:09 -0400

    Finished writing functions - not yet tested

--- src/my_functions.cpp
+++ src/my_functions.cpp
@@ -1,16 +1,28 @@
 #include "my_functions.h":
 
 std::string createString(){
-
+	return std::string("Hello world!");
 }
 
 std::vector<std::string> createVector(){
+	return std::vector<std::string>();
 }
 
 int putStringsInVector(int num){
+	std::vector<std::string> myVector = createVector();
+	for(int i = 0; i < num; ++i){
+		myVector.push_back(createVector());
+	}
+	return myVector.size();
 }
 
 void modifyVector(std::vector<int> & vector){
-
+	for(int i = 0; i < vector.size(); ++i){
+		// The 5th element corresponds to index i=4
+		int elementNum = i + 1; 
+		if( (i % 5) == 0 ){
+			vector[i] = 42;	
+		}	
+	}	
 }
 
