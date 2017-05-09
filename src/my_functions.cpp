457f728fe43f19f4ed7033e86cfff96b1afae5fe
Samantha Teskey <teskeys@mail.gvsu.edu>
2017-05-09 14:23:50 -0400

    All tests are now passing

--- src/my_functions.cpp
+++ src/my_functions.cpp
@@ -1,4 +1,4 @@
-#include "my_functions.h":
+#include "my_functions.h"
 
 std::string createString(){
 	return std::string("Hello world!");
@@ -11,7 +11,7 @@ std::vector<std::string> createVector(){
 int putStringsInVector(int num){
 	std::vector<std::string> myVector = createVector();
 	for(int i = 0; i < num; ++i){
-		myVector.push_back(createVector());
+		myVector.push_back(createString());
 	}
 	return myVector.size();
 }
@@ -19,7 +19,6 @@ int putStringsInVector(int num){
 void modifyVector(std::vector<int> & vector){
 	for(int i = 0; i < vector.size(); ++i){
 		// The 5th element corresponds to index i=4
-		int elementNum = i + 1; 
 		if( (i % 5) == 0 ){
 			vector[i] = 42;	
 		}	
