e9a0c64b4766a234b5517468876c430b8728ac2c
Samantha Teskey <teskeys@mail.gvsu.edu>
2017-05-09 18:48:21 -0400

    Adjusted formatting to comply with CIS standards, addressed unsigned/signed comparison warning

--- src/my_functions.cpp
+++ src/my_functions.cpp
@@ -1,27 +1,37 @@
 #include "my_functions.h"
 
 std::string createString(){
-	return std::string("Hello world!");
+    //Creates a string and returns it by value
+    return std::string("Hello world!");
 }
 
 std::vector<std::string> createVector(){
-	return std::vector<std::string>();
+    //Creates a vector of strings and returns it by value
+    return std::vector<std::string>();
 }
 
 int putStringsInVector(int num){
-	std::vector<std::string> myVector = createVector();
-	for(int i = 0; i < num; ++i){
-		myVector.push_back(createString());
-	}
-	return myVector.size();
+    //Initializes a vector to the vector returned by createVector()
+    std::vector<std::string> myVector = createVector();
+
+    /** 
+      Calls createString num times and appends the returned string
+      to the vector each time.
+    */
+    for(int i = 0; i < num; ++i){
+        myVector.push_back(createString());
+    }
+
+    //Returns final size of the vector, which should equal num
+    return myVector.size();
 }
 
 void modifyVector(std::vector<int> & vector){
-	for(int i = 0; i < vector.size(); ++i){
-		// The 5th element corresponds to index i=4
-		if( (i % 5) == 0 ){
-			vector[i] = 42;	
-		}	
-	}	
+    //Loops through all the integers contained in referenced vector
+    for(int i = 0; i < static_cast<int>(vector.size()); ++i){
+        //If the index is divisible by 5, changes the contents to 42
+        if( (i % 5) == 0 ){
+            vector[i] = 42;	
+        }
+    }	
 }
-
